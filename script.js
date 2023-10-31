e15b3a49b8b6c0de6ae94f5947566b1390beab6e
JHoldsworth23 <jacobph101@icloud.com>
2023-10-31 17:08:20 +0000

    Refactor the getComputerChoice function code

--- script.js
+++ script.js
@@ -1,12 +1,5 @@
 function getComputerChoice() {
-    switch (Math.floor(Math.random() * 3)) {
-        case 0:
-            return 'rock';
-        case 1: 
-            return 'paper';
-        case 2: 
-            return 'scissors';
-    }
+    return ['rock', 'paper', 'scissors'][Math.floor(Math.random() * 3)];
 }
 
 function determineOutcome(playerSelection, computerSelection) {
