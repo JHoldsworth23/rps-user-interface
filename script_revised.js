775836c8d88accc8a4eafcbdab7efcb6a3ad697f
JHoldsworth23 <jacobph101@icloud.com>
2023-11-01 15:28:12 +0000

    Display the element emoji for player and computer choice

--- script_revised.js
+++ script_revised.js
@@ -1,5 +1,16 @@
 function getComputerChoice() {
-    return ['water', 'fire', 'earth'][Math.floor(Math.random() * 3)];
+    return ["water", "fire", "earth"][Math.floor(Math.random() * 3)];
+}
+
+function emojiElement(element) {
+    switch (element) {
+        case "water":
+            return "💧";
+        case "fire":
+            return "🔥";
+        case "earth":
+            return "🍃";
+    }
 }
 
 function determineOutcome(playerSelection, computerSelection) {
@@ -7,8 +18,8 @@ function determineOutcome(playerSelection, computerSelection) {
         (playerSelection === "water" && computerSelection === "fire") || 
         (playerSelection === "fire" && computerSelection === "earth") || 
         (playerSelection === "earth" && computerSelection === "water") ? 
-        `You win this round!\n ${playerSelection.toUpperCase()} beats ${computerSelection.toUpperCase()}` :
-        `You lose this round!\n ${playerSelection.toUpperCase()} is beaten by ${computerSelection.toUpperCase()}`;
+        `You win this round! ${playerSelection.toUpperCase()} beats ${computerSelection.toUpperCase()}` :
+        `You lose this round! ${playerSelection.toUpperCase()} is beaten by ${computerSelection.toUpperCase()}`;
 }
 
 function finalMessage() {
@@ -17,33 +28,39 @@ function finalMessage() {
         : "Your computer is the Master of Elements";
 }
 
-const water_element = document.querySelector('#water');
-const fire_element = document.querySelector('#fire');
-const earth_element = document.querySelector('#earth');
+const water_element = document.querySelector("#water");
+const fire_element = document.querySelector("#fire");
+const earth_element = document.querySelector("#earth");
 
-const result = document.querySelector('.result-text');
-const roundResult = document.createElement('h3');
+const result = document.querySelector(".result-text");
+const roundResult = document.createElement("h3");
 
-const playerElement = document.querySelector('.player-choice');
-const computerElement = document.querySelector('.computer-choice')
+const playerElement = document.querySelector(".player-choice");
+const computerElement = document.querySelector(".computer-choice")
 
-water_element.addEventListener('click', () => {
+water_element.addEventListener("click", () => {
+    const playerSelection = 'water';
     const computerSelection = getComputerChoice();
-    roundResult.textContent = determineOutcome('water', computerSelection);
-    playerElement.textContent = '💧';
+    roundResult.textContent = determineOutcome(playerSelection, computerSelection);
+    playerElement.textContent = emojiElement(playerSelection);
+    computerElement.textContent = emojiElement(computerSelection);
     result.appendChild(roundResult);
 });
 
-fire_element.addEventListener('click', () => {
+fire_element.addEventListener("click", () => {
+    const playerSelection = 'fire';
     const computerSelection = getComputerChoice();
-    roundResult.textContent = determineOutcome('fire', computerSelection);
-    playerElement.textContent = '🔥';
+    roundResult.textContent = determineOutcome(playerSelection, computerSelection);
+    playerElement.textContent = emojiElement(playerSelection);
+    computerElement.textContent = emojiElement(computerSelection);
     result.appendChild(roundResult);
 });
 
-earth_element.addEventListener('click', () => {
+earth_element.addEventListener("click", () => {
+    const playerSelection = 'earth';
     const computerSelection = getComputerChoice();
-    roundResult.textContent = determineOutcome('earth', computerSelection);
-    playerElement.textContent = '🍃';
+    roundResult.textContent = determineOutcome(playerSelection, computerSelection);
+    playerElement.textContent = emojiElement(playerSelection);
+    computerElement.textContent = emojiElement(computerSelection);
     result.appendChild(roundResult);
 });
